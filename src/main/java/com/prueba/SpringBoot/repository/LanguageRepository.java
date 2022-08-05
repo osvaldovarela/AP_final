4341bbb4db76252c530bca6a33bd7cd77a3f2c7a
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2022-08-05 05:52:00 -0300

    clear

--- src/main/java/com/prueba/SpringBoot/repository/LanguageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prueba.SpringBoot.repository;
-
-import com.prueba.SpringBoot.entity.Language;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LanguageRepository extends JpaRepository<Language, Long> {
-
-}
